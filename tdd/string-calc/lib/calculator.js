61d896bcb2b57f8b05ac07fedf2df99fd9640c2a
Brigette Lundeen <brlundee@microsoft.com>
2019-10-10 10:36:14 -0700

    continuing on string calculator

--- tdd/string-calc/lib/calculator.js
+++ tdd/string-calc/lib/calculator.js
@@ -1,8 +1,22 @@
 function Calculator() {
 }
 
-Calculator.prototype.add = function(a, b) {
-    return a + b;
+Calculator.prototype.add = function(nums) {
+    // is 0 if there is an empty string supplied
+    if(nums.length === 0) return 0;
+    
+    var numArr = nums.split(/\s*(?:[,;]|$)\s*/g);
+    console.log(numArr);
+
+    // is the number if there is only one number supplied
+    if(numArr.length === 1) return parseInt(numArr[0]);
+
+    // add up numbers if more than one number supplied
+    var total = 0;
+    numArr.forEach((num) => {
+        total += parseInt(num);
+    })
+    return total;
 }
 
 
--- tdd/string-calc/spec/calculator.js
+++ tdd/string-calc/spec/calculator.js
@@ -6,7 +6,31 @@ describe("Calculator", function() {
     calc = new Calc();
   });
 
-  it("`2 + 3` should return 5", function() {
-    expect(calc.add(2, 3)).toEqual(5);
+  it("`` should return 0", function() {
+    expect(calc.add("")).toEqual(0);
+  });
+
+  it("`1` should return 1", function() {
+    expect(calc.add("1")).toEqual(1);
+  });
+
+  it("`1,1` should return 2", function() {
+    expect(calc.add("1,1")).toEqual(2);
+  });
+  
+  it("`1,1, 3` should return 5", function() {
+    expect(calc.add("1,1, 3")).toEqual(5);
+  });
+  
+  it("`1,4,-3` should return 2", function() {
+    expect(calc.add("1,4,-3")).toEqual(2);
+  });
+
+  it("`1;1,1` should return 3", function() {
+    expect(calc.add("1;1,1")).toEqual(3);
+  });
+  
+  xit("`1;1\\n13` should return 15", function() {
+    expect(calc.add("1;1\\n13")).toEqual(15);
   });
 });
